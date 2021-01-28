8e4bdf1a7e8361e78a3e140dda5fc3b9c5dad13c
Gaurav Gaur <70309280+gaurav-gaur@users.noreply.github.com>
2021-01-28 13:53:01 +0530

    Day 1 Improved Solution
    Solution for Rearrange Geek and his Classmates
    Each of the N students in the class can be identified by a unique roll number X and each
    desk has a number i associated with it. Only one student can sit on one desk.
    Each time the teacher turns her back, a student with roll number X sitting on desk number i
    gets up and takes the place of the student with roll number i.
    If the current position of N students in the class is given to you in an array, such that i is the
     desk number and a[i] is the roll number of the student sitting on the desk, can you modify
    //  a[ ] to represent the next position of all the students ?

--- GfG30DOCSolutions/Day1RearrangePrank/Day1RearrangePrank.cpp
+++ GfG30DOCSolutions/Day1RearrangePrank/Day1RearrangePrank.cpp
@@ -13,52 +13,46 @@ using namespace std;
 
 class Solution {
 public:
-    // Time Complexity: O(n), space complexity O(n)
+    // Time complexity: O(n), Auxiliary Space: O(1)
     void prank(long long a[], int n) {
-        // code here
-        // element index map
-        unordered_map<int, int> a_i;
-        // fill element index map
+        assert(n > 0);
+        
+        // So here in simple terms we are saying
+        // that at any given position whatever roll no
+        // is stored, value of roll no at that index 
+        // should be copied to it
+        // i - position to be updated
+        // a[i] - current roll no at i
+        // a[a[i]] - element at position a[i]
+        
+        // Now the problem is if we update
+        // a[i] = a[a[i]] we lose the value at a[i]
+        // So that whenever we have to updates the position
+        // whose value is i, then we'll not have original a[i]
+        // as that has already been over written
+
+        // To solve that whatever value we are trying to store at where a
+        // is stored , we'll store along with value of a, so that we don't lose it
+        // a + b * n
+
+
+        // in scenario where 
+        // a should get b, and c should get a        
+        // At a's place, store a + (b % n) * n
+        // At c's place, store c + (a + (b % n) * n)%n*n => c + (a % n)*n because ((b % n)*n)%n will be 0
+
+        // so to just get b at a, and a at c, if we divide everything by n
+        // then a / n -> 0 because a lies between 0 to n-1 and only b remain there, and b will be stored at a
+        // similary it will work for a at c.
+        // Putting it all together
         for (int i = 0; i < n; i++) {
-            a_i[a[i]] = i;
+            a[i] += (a[a[i]] % n) * n;
         }
-        // to keep things simple, use a temp array
-        int* b = new int[n];  
-        // core shuffle logic
+
         for (int i = 0; i < n; i++) {
-            // a_i[i] will give position at which
-            // roll number i is placed, then
-            // place a[i] i.e. current element at
-            // that place.
-            b[a_i[i]] = a[i];
-        }
-        // copy elements in original array
-        for (int i = 0; i < n; i++) {
-            a[i] = b[i];
+            a[i] /= n;
         }
     }
-
-    //void swapOneLoop(long long a[], int n, int index) {
-    //    int startIndex = index;
-    //    long long startIndexValue = a[index];
-    //    do {
-    //        int nextIndex = (int)a[index];
-    //        if (nextIndex == startIndex) {
-    //            a[index] = startIndexValue;
-    //        }
-    //        else {
-    //            a[index] = a[nextIndex];
-    //        }
-    //        index = nextIndex;
-    //    } while (index != startIndex);
-    //}
-
-    //// Time complexity: O(n), Auxiliary Space: O(1)
-    //void prank(long long a[], int n) {
-    //    assert(n > 0);
-
-
-    //}
 };
 
 // { Driver Code Starts.
