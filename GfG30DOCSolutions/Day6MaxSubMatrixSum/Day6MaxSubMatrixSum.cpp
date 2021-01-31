4ee28d2c05b7522368d51eb3cb2f4a25aab4a0d3
Gaurav Gaur <70309280+gaurav-gaur@users.noreply.github.com>
2021-02-01 01:22:26 +0530

    Day 6 Solution
    Day 6 Solution for problem
    In Geekland there is a grid of coins of size N x N. You have to find the maximum sum of coins in any sub-grid of size K x K.
    Note: Coins of the negative denomination are also possible at Geekland.
    Your Task:
    You don't need to read input or print anything. Complete the function Maximum_Sum() which takes the matrix mat[], size of Matrix N, and value K as input parameters and returns the maximum sum.
    
    Expected Time Complexity: O(N2)
    Expected Auxiliary Space: O(N2)
    
    Constraints:
    1 ≤ K ≤ N ≤ 103
    -5*105 ≤ mat[i][j] ≤ 5*105
    
    Company Tags

---
+++ GfG30DOCSolutions/Day6MaxSubMatrixSum/Day6MaxSubMatrixSum.cpp
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+#include<iostream>
+#include<cassert>
+#include<vector>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution {
+
+public:
+    // Sum of a given column in sub matrix
+    // Ex:
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+    // for top left index 0, 0 and k=2
+    // column sum = mat[0][0] + mat[1][0] = ;
+    int colSum(const vector<vector<int> >& mat, int row, int col, int K) {
+        int colSum = 0;
+        for (int i = row; i < row + K; i++) {
+            colSum += mat[i][col];
+        }
+        return colSum;
+    }
+
+    int Maximum_Sum(vector<vector<int> >& mat, int N, int K) {
+        // Your code goes here
+        assert(N >= K);
+        // In order to avoid calculating same cells again and again
+        // we'll use rolling sum technique
+        // For that we need to preprocess the sum of vertical strips
+        // of height K and width 1
+        vector<vector<int> > kMat(N-K+1);
+        // Number of rows will be N - K + 1, Number of cols will be N
+        for (int i = 0; i < N - K + 1; i++) {
+            kMat[i] = vector<int>(N);
+        }
+        
+        for (int i = 0; i < N - K + 1; i++) {
+            for (int j = 0; j < N; j++) {
+                kMat[i][j] = colSum(mat, i, j, K);
+            }
+        }
+
+        int maxSubMatSum = -INFINITY;
+
+        for (int i = 0; i < N - K + 1; i++) {
+            int currSubMatSum = 0;
+            for (int j = 0; j < K; j++) {
+                currSubMatSum += kMat[i][j];
+            }
+            if (currSubMatSum > maxSubMatSum) {
+                maxSubMatSum = currSubMatSum;
+            }
+            for (int j = 1; j < N - K + 1; j++) {
+                currSubMatSum = currSubMatSum + kMat[i][j + K - 1] - kMat[i][j-1];
+                if (currSubMatSum > maxSubMatSum) {
+                    maxSubMatSum = currSubMatSum;
+                }
+            }
+        }
+        return maxSubMatSum;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(N, 0));
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cin >> mat[i][j];
+            }
+        }
+
+        int k; cin >> k;
+        Solution obj;
+        cout << obj.Maximum_Sum(mat, N, k) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
